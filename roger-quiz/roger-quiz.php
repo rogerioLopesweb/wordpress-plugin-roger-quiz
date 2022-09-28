000b99b9ebe29d49900baee2d13787edf959c972
Rogerio Lopes <113315029+rogerioLopesweb@users.noreply.github.com>
2022-09-28 15:11:48 -0300

    Arquivos iniciais

---
+++ roger-quiz/roger-quiz.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: Roger Quiz
+
+Description: Cria sua perguntas e respostas
+
+Version: 1.0
+
+Author: TECH LEAD - Rogério Lopes
+
+Author URI: https://www.linkedin.com/in/rogerio-tech-lead/
+
+License: GPL2
+
+*/
+// inclui as rotinas do plugin
+require_once plugin_dir_path(__FILE__) . '/includes/roger-quiz-admin.php';
+require_once plugin_dir_path(__FILE__) . '/includes/roger-quiz-init.php';
+require_once plugin_dir_path(__FILE__) . '/includes/roger-quiz-list.php';
+require_once plugin_dir_path(__FILE__) . '/includes/roger-quiz-single.php';
+require_once plugin_dir_path(__FILE__) . '/includes/roger-quiz-shortcode.php';
+
+
+?>
